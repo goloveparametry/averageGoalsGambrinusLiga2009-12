43ca9c314d306c67490dfcecf817d5bb981b4dca
Radim Sevcik <radim.sevcik@goloveparametry.cz>
2012-07-13 22:12:26 +0200

    Detail analysis added

--- code/averageGoals.r
+++ code/averageGoals.r
@@ -8,21 +8,25 @@ gl <- read.csv("data/gl2009-12.csv", quote="")
 # "Team_Away"	Away team name
 # "Note"	Match note
 
+# remove 184 obs. (DNF)
+gl <- gl[-184,c(3:6)]
+
+# rename the column names
+colnames(gl) <- c("gh","ga","teamh","teama")
+
 # make xlabels vertical
 par(las = 3, mar=c(10,2,2,2))
 
-# remove 184 obs. (DNF)
-cze09 <- gl[-184,c(3:6)]
-colnames(cze09) <- c("g1","g2","team1","team2")
-
 # avg goals scored home
-plot(aggregate(cze09$g1, list(cze09$team1),mean), type="p", xlab="", main="avg goals scored home: GL 2009-2012") 
+plot(aggregate(gl$gh, list(cze09$teamh),mean), type="p", xlab="", main="avg goals scored home: GL 2009-2012") 
 
 # avg goals conceded away
-plot(aggregate(cze09$g1, list(cze09$team2),mean), type="p", xlab="", main="avg goals conceaded away: GL 2009-2012") 
+plot(aggregate(gl$gh, list(cze09$teama),mean), type="p", xlab="", main="avg goals conceaded away: GL 2009-2012") 
 
 # avg goals scored away
-plot(aggregate(cze09$g2, list(cze09$team2),mean), type="p", xlab="", main="avg goals scored away: GL 2009-2012") 
+plot(aggregate(gl$ga, list(cze09$teama),mean), type="p", xlab="", main="avg goals scored away: GL 2009-2012") 
 
 # avg goals conceded home
-plot(aggregate(cze09$g2, list(cze09$team1),mean), type="p", xlab="", main="avg goals conceaded home: GL 2009-2012") 
+plot(aggregate(gl$ga, list(cze09$teamh),mean), type="p", xlab="", main="avg goals conceaded home: GL 2009-2012") 
+
+
