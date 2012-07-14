408adf9fdcc5ab9de124e29b2a6659cdb207682d
Radim Sevcik <radim.sevcik@goloveparametry.cz>
2012-07-14 10:17:27 +0200

    Code cleaning, Details edit

--- code/averageGoals.r
+++ code/averageGoals.r
@@ -1,4 +1,4 @@
-gl <- read.csv("data/gl2009-12.csv", quote="")
+gl09f <- read.csv("data/gl2009-12.csv", quote="") #full dataset
 
 # "Date" 	Date of match played
 # "Time" 	Time of match played
@@ -9,24 +9,37 @@ gl <- read.csv("data/gl2009-12.csv", quote="")
 # "Note"	Match note
 
 # remove 184 obs. (DNF)
-gl <- gl[-184,c(3:6)]
+gl09 <- gl09f[-184,c(3:6)]
 
 # rename the column names
-colnames(gl) <- c("gh","ga","teamh","teama")
+colnames(gl09) <- c("gh","ga","teamh","teama")
 
 # make xlabels vertical
 par(las = 3, mar=c(10,2,2,2))
 
 # avg goals scored home
-plot(aggregate(gl$gh, list(cze09$teamh),mean), type="p", xlab="", main="avg goals scored home: GL 2009-2012") 
+plot(aggregate(gl09$gh, list(gl09$teamh),mean), type="p", xlab="", main="avg goals scored home: GL 2009-2012") 
 
 # avg goals conceded away
-plot(aggregate(gl$gh, list(cze09$teama),mean), type="p", xlab="", main="avg goals conceaded away: GL 2009-2012") 
+plot(aggregate(gl09$gh, list(gl09$teama),mean), type="p", xlab="", main="avg goals conceaded away: GL 2009-2012") 
 
 # avg goals scored away
-plot(aggregate(gl$ga, list(cze09$teama),mean), type="p", xlab="", main="avg goals scored away: GL 2009-2012") 
+plot(aggregate(gl09$ga, list(gl09$teama),mean), type="p", xlab="", main="avg goals scored away: GL 2009-2012") 
 
 # avg goals conceded home
-plot(aggregate(gl$ga, list(cze09$teamh),mean), type="p", xlab="", main="avg goals conceaded home: GL 2009-2012") 
+plot(aggregate(gl09$ga, list(gl09$teamh),mean), type="p", xlab="", main="avg goals conceaded home: GL 2009-2012") 
 
+# average goals scored by Sparta Prague and Viktoria Plzen at home
+mean(gl09[gl09$teamh == "Sparta Praha",1])
+mean(gl09[gl09$teamh == "Viktoria Plzeň",1])
 
+# games when Viktoria Plzen did not score at home
+gl09f[gl09f$Team_H == "Viktoria Plzeň" & gl09f$H == 0,]
+
+# average goals scored by FK Jablonec, Viktoria Plzen and Sparta Prague away
+mean(gl09[gl09$teama == "FK Jablonec",2])
+mean(gl09[gl09$teama == "Viktoria Plzeň",2])
+mean(gl09[gl09$teama == "Sparta Praha",2])
+
+# games when Sparta Praha conceded at home
+gl09f[gl09f$Team_H == "Sparta Praha" & gl09f$A != 0,]
